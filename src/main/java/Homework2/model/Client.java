dd8f5cb158feff2031c5ee0fd249c7bfe5dd969c
Anatomica <maksfomin@gmail.com>
2020-07-03 23:02:23 +0300

    Start HW 3

---
+++ src/main/java/Homework2/model/Client.java
@@ -0,0 +1,45 @@
+package Homework2.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "Name")
+    private String name;
+
+    public Client() {
+    }
+
+    public Client(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("SimpleItem [id = %d, Name = %s]", id, name);
+    }
+}
